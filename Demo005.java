fd0c0dc20e652327243b3bb6cb01a040b2e3d022
sathwik-11-creater <msathwik876@gmail.com>
2026-06-29 11:51:42 +0530

    Username and Password

---
+++ Demo005.java
@@ -0,0 +1,11 @@
+class Demo005{
+    public static void main(String[] args){
+        String name = "Sathwik";
+        int pass = 12345;
+        while((name == "Sathwik") && (pass == 12345)){
+             System.out.println("user and password is valid");
+             break;
+        }
+
+    }
+}
