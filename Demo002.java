721f4f50e912e52788f55510a55ebac76c6a51e3
sathwik-11-creater <msathwik876@gmail.com>
2026-06-29 11:49:25 +0530

    Greater Number

---
+++ Demo002.java
@@ -0,0 +1,12 @@
+public class Demo002 {
+    public static void main(String[] args) {
+        int a=10;
+        int b=5;
+        if(a>b){
+            System.out.println(a+" is greater then " + b);
+        }else{
+                System.out.println(b+"is greater then " + a);
+        }
+    }
+
+}
