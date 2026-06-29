76a4444deff226f381b1d1cf6090f31b75e59a42
sathwik-11-creater <msathwik876@gmail.com>
2026-06-29 11:53:11 +0530

    Name, Age, Number

---
+++ Demo007.java
@@ -0,0 +1,11 @@
+public class Demo007 {
+    public static void main(String[] args) {
+        String Name = "Sathwik";
+        int Age = 20;
+        long PhNO = 1234567890;
+        while ((Name == "Sathwik") && (Age == 20) && (PhNO == 1234567890)) {
+            System.out.println("Hello Sathwik");
+            break;
+        }
+    }
+}
