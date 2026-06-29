75ab5b69d2549d4949a3faefa8a64365a547b6d0
sathwik-11-creater <msathwik876@gmail.com>
2026-06-29 11:50:20 +0530

    Greater Amoung 3 Numbers

---
+++ Demo003.java
@@ -0,0 +1,17 @@
+class Demo003{
+    public static void main(String[] args){
+        int a=101;
+        int b=20;
+        int c=30;
+    if((a>b) && (a>c)){
+        System.out.println("a is greater");
+
+    }
+    else if((b>a) && (b>c)){
+        System.out.println("b is greater");
+    }
+    else{
+        System.out.println("c is greater");
+    }
+    }
+}
